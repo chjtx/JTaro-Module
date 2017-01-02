a382ff6832144caf0820636fd4e62162debbd4d9
BarZu <cjlice@qq.com>
2017-01-02 22:13:39 +0800

    添加支持html/css/plain引入

--- demos/x/x.js
+++ demos/x/x.js
@@ -1,5 +1,11 @@
 import '../b.js'
+import tpl from '../a.html'
 import * as a from '../a.js'
+import '../a.css'
+import txt from './123.txt'
+
 export var x = 'xxx'
 
 console.log(a)
+console.log(txt)
+document.getElementById('jtaro_app').innerHTML = tpl
--- src/client.js
+++ src/client.js
@@ -1,16 +1,27 @@
-/*! JTaro-Module client.js v0.0.1 ~ (c) 2017 Author:BarZu Git:https://github.com/chjtx/JTaro-Module/ */
-/* global */
+/*! JTaro-Module client.js v0.0.2 ~ (c) 2017 Author:BarZu Git:https://github.com/chjtx/JTaro-Module/ */
+/* global XMLHttpRequest */
 (function () {
   window.JTaroModules = {}
 
   var loader = {
+    // 同步加载
+    ajax: function (path, callback) {
+      var xhr = new XMLHttpRequest()
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+          callback(xhr.responseText)
+        }
+      }
+      xhr.open('GET', path, false)
+      xhr.send()
+    },
     // 路径处理
     path: {
       dirname: function (p) {
         return p.substr(0, p.lastIndexOf('/'))
       },
       resolve: function (p) {
-        var currentScript = document.currentScript.src
+        var currentScript = document.currentScript.src || document.baseURI
         var d = this.dirname(currentScript)
         var path
 
@@ -47,31 +58,109 @@
       }
       return exist
     },
-    // 引入模块
-    import: function (path, callback) {
+    importJs: function (result, callback) {
       var me = this
-      var src = this.path.resolve(path)
       var script
-
-      if (!me.isExist(src.path)) {
+      if (!me.isExist(result.path)) {
         script = document.createElement('script')
-        script.src = src.src
+        script.src = result.src
         script.onload = function () {
-          if (typeof callback === 'function') callback(src)
+          if (typeof callback === 'function') callback(result)
         }
         script.onerror = function (e) {
-          console.error('`JTaroLoader.import(\'' + src.src + '\', g)` load fail from ' + src.from)
+          console.error('`JTaroLoader.import(\'' + result.src + '\', g)` load fail from ' + result.from)
         }
         setTimeout(function () {
           // 防止多次引入同一模块
-          if (!me.isExist(src.path)) {
+          if (!me.isExist(result.path)) {
             document.head.appendChild(script)
           } else {
-            callback(src)
+            callback(result)
           }
         }, 0)
       } else if (typeof callback === 'function') {
-        callback(src)
+        callback(result)
+      }
+    },
+    // 将路径转换成id
+    path2id: function (path) {
+      return path.substr(0, path.lastIndexOf('.')).replace(/\//g, '_')
+    },
+    importHtml: function (result, callback) {
+      var me = this
+      this.ajax(result.src, function (data) {
+        var reg = /<style>([\s\S]+)<\/style>/
+        var styleText = reg.exec(data)
+        var style
+        var css
+        var id = me.path2id(result.src)
+
+        // 将模板的<style>提取到head
+        if (styleText) {
+          style = document.getElementById('jtaro_style' + id)
+          if (!style) {
+            css = '\n[jtaro' + id + '] ' + styleText[1].trim()
+              .replace(/}\s+(?!$)/g, '}\n[jtaro' + id + '] ')
+              .split(/,\s+/).join(',\n[jtaro' + id + '] ') + '\n'
+            style = document.createElement('style')
+            style.id = 'jtaro_style' + id
+            style.innerHTML = css
+            document.head.appendChild(style)
+          }
+
+          // 过滤已截取的style
+          data = data.replace(styleText[0], '')
+
+          // 去除行首空格
+          data = data.replace(/^\s+/, '')
+
+          // 标识第一个dom
+          data = data.replace(/^<\w+(?= |>)/, function (match) {
+            return match + ' jtaro' + id + ' '
+          })
+        }
+
+        window.JTaroModules[result.src] = { default: data }
+        callback(result)
+      })
+    },
+    importCss: function (result, callback) {
+      var id = 'jtaro_link' + this.path2id(result.src)
+      var link = document.getElementById(id)
+      if (!link) {
+        link = document.createElement('link')
+        link.id = id
+        link.rel = 'stylesheet'
+        link.href = result.src
+        document.head.appendChild(link)
+      }
+      callback(result)
+    },
+    importOther: function (result, callback) {
+      this.ajax(result.src, function (data) {
+        window.JTaroModules[result.src] = { default: data }
+        callback(result)
+      })
+    },
+    // 引入模块
+    import: function (path, callback) {
+      var result = this.path.resolve(path)
+
+      // js
+      if (/\.js$/.test(result.src)) {
+        this.importJs(result, callback)
+
+      // html
+      } else if (/\.html$/.test(result.src)) {
+        this.importHtml(result, callback)
+
+      // css
+      } else if (/\.css$/.test(result.src)) {
+        this.importCss(result, callback)
+
+      // other
+      } else {
+        this.importOther(result, callback)
       }
     }
   }
